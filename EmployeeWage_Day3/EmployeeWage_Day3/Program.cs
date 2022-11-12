9478318578d53460ddefdc8d5371772b1229aaac
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-12 19:32:03 +0530

    [Suraj]Add:GreeetingMessage

---
+++ EmployeeWage_Day3/EmployeeWage_Day3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWage
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage: ");
+        }
+    }
+}
