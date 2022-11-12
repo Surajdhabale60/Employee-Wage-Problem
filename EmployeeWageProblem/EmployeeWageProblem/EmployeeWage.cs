17cebc15806b06b682844361eca1d8fe73f7e260
Suraj Dhabale <surajdhabale60@gmail.com>
2022-11-12 20:50:37 +0530

    [Suraj]Add:PartTime

--- EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
+++ EmployeeWageProblem/EmployeeWageProblem/EmployeeWage.cs
@@ -45,5 +45,33 @@ namespace EmployeeWageProblem
             Empwage = empHrs * WAGE_PER_HR;
             Console.WriteLine("Employee salary is :" + Empwage);
         }
+
+        public void PartTime()
+        {
+            int empHrs = 0;
+            int Empwage = 0;
+
+            Random random = new Random();
+            int empcheck = random.Next(0, 3);
+            if (empcheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else if (empcheck == IS_PART_TIME)
+            {
+                Console.WriteLine("employee is PartTime");
+                empHrs = 8;
+            }
+
+            else
+            {
+                Console.WriteLine("employee is Absent");
+                empHrs = 0;
+            }
+
+            Empwage = empHrs * WAGE_PER_HR;
+            Console.WriteLine("Employee salary is :" + Empwage);
+        }
     }
 }
